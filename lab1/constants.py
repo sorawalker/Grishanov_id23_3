e7802d2dbe2d7bb52e4109fcdfdfa0be9421aee7
sorawalker <nikitka.grishanov@gmail.com>
2024-11-13 15:52:06 +0300

    refactor: All files to main.py

--- lab1/constants.py
+++
@@ -1,6 +0,0 @@
-WINDOW_HEIGHT = 600
-WINDOW_WIDTH = 600
-CIRCLE_RADIUS = 200
-DOT_RADIUS = 10
-DOT_DIRECTION = 1  # 1 - по часовой, -1 против часовой
-DOT_SPEED = 5
--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,12 @@
 from tkinter import *
-from constants import *
 from math import cos, sin, radians
+
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
+CIRCLE_RADIUS = 200
+DOT_RADIUS = 10
+DOT_DIRECTION = 1  # 1 - по часовой, -1 против часовой
+DOT_SPEED = 5
 
 
 class MainCanvas(Canvas):
